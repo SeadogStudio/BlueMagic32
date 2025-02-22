471b2c929a7a54ce78a5685feaeb864205fd3eca
SeadogStudio <yort136@gmail.com>
2025-02-22 10:44:34 +0700

    Update BlueMagicState.cpp

--- src/BlueMagicState.cpp
+++ src/BlueMagicState.cpp
@@ -1,263 +1,213 @@
 #include "BlueMagicState.h"
+#include <stdlib.h> // For atoi
 
 BlueMagicState *BlueMagicState::instance = 0;
 
-BlueMagicState *BlueMagicState::getInstance()
-{
-    if (instance == 0)
-    {
+BlueMagicState *BlueMagicState::getInstance() {
+    if (instance == 0) {
         instance = new BlueMagicState();
     }
-
     return instance;
 }
 
-BlueMagicState::BlueMagicState()
-{
+BlueMagicState::BlueMagicState() : _timecode("00:00:00:00"), _timecodeRaw(0), _transportMode(0), _aperture(0.0f), _focus(0.0f), _zoom(0), _iso(0), _shutter(0), _codec(0), _quality(0), _frameRate(0), _sensorFrameRate(0), _frameWidth(0), _frameHeight(0), _flags(0), _whiteBalance(0), _tint(0), _status(0) {
+    // Initialize all member variables
 }
 
-void BlueMagicState::setChanged(bool changed)
-{
+void BlueMagicState::setChanged(bool changed) {
     _changed = changed;
 }
 
-bool BlueMagicState::changed()
-{
+bool BlueMagicState::changed() {
     bool changed = _changed;
     _changed = false;
     return changed;
 }
 
-void BlueMagicState::settingsNotify(bool changed, uint8_t *data)
-{
+void BlueMagicState::settingsNotify(bool changed, uint8_t *data) {
     _settingsChanged = changed;
-    _settingsData = data;
+    _settingsData = data; // Assumes data is valid and externally managed
 }
 
-bool BlueMagicState::settingsChanged()
-{
+bool BlueMagicState::settingsChanged() {
     bool changed = _settingsChanged;
     _settingsChanged = false;
     return changed;
 }
 
-uint8_t *BlueMagicState::settingsData()
-{
+uint8_t *BlueMagicState::settingsData() {
     return _settingsData;
 }
 
-void BlueMagicState::timecodeNotify(bool changed, uint8_t *data)
-{
+void BlueMagicState::timecodeNotify(bool changed, uint8_t *data) {
     _timecodeChanged = changed;
-    _timecodeData = data;
+    _timecodeData = data; // Assumes data is valid and externally managed
 }
 
-bool BlueMagicState::timecodeChanged()
-{
+bool BlueMagicState::timecodeChanged() {
     bool changed = _timecodeChanged;
     _timecodeChanged = false;
     return changed;
 }
 
-uint8_t *BlueMagicState::timecodeData()
-{
+uint8_t *BlueMagicState::timecodeData() {
     return _timecodeData;
 }
 
-void BlueMagicState::statusNotify(bool changed, uint8_t *data)
-{
+void BlueMagicState::statusNotify(bool changed, uint8_t *data) {
     _statusChanged = changed;
-    _statusData = data;
+    _statusData = data; // Assumes data is valid and externally managed
 }
 
-bool BlueMagicState::statusChanged()
-{
+bool BlueMagicState::statusChanged() {
     bool changed = _statusChanged;
     _statusChanged = false;
     return changed;
 }
 
-uint8_t *BlueMagicState::statusData()
-{
+uint8_t *BlueMagicState::statusData() {
     return _statusData;
 }
 
-void BlueMagicState::setCameraStatus(uint8_t status)
-{
+void BlueMagicState::setCameraStatus(uint8_t status) {
     _status = status;
 }
 
-uint8_t BlueMagicState::getCameraStatus()
-{
+uint8_t BlueMagicState::getCameraStatus() {
     return _status;
 }
 
-void BlueMagicState::setTimecode(uint8_t H, uint8_t M, uint8_t S, uint8_t f)
-{
-    char timecode[12] = "00:00:00:00";
-    char timecodeR[10] = "00000000";
+void BlueMagicState::setTimecode(uint8_t H, uint8_t M, uint8_t S, uint8_t f) {
+    char timecode[12];
     sprintf(timecode, "%02d:%02d:%02d:%02d", H, M, S, f);
-    sprintf(timecodeR, "%02d%02d%02d%02d", H, M, S, f);
-    _timecode = String(timecode);
-    _timecodeRaw = String(timecodeR).toInt();
+    _timecode = timecode; // Changed to character array
+    _timecodeRaw = atoi(timecode + 0) * 1000000 + atoi(timecode + 3) * 10000 + atoi(timecode + 6) * 100 + atoi(timecode + 9);
 }
 
-String BlueMagicState::getTimecode()
-{
-    return _timecode;
+String BlueMagicState::getTimecode() {
+    return String(_timecode); // Returns a String version
 }
 
-uint32_t BlueMagicState::getTimecodeRaw()
-{
+uint32_t BlueMagicState::getTimecodeRaw() {
     return _timecodeRaw;
 }
 
-void BlueMagicState::setTransportMode(int8_t mode)
-{
+void BlueMagicState::setTransportMode(int8_t mode) {
     _transportMode = mode;
 }
 
-int8_t BlueMagicState::getTransportMode()
-{
+int8_t BlueMagicState::getTransportMode() {
     return _transportMode;
 }
 
-void BlueMagicState::setAperture(float aperture)
-{
+void BlueMagicState::setAperture(float aperture) {
     _aperture = aperture;
 }
 
-float BlueMagicState::getAperture()
-{
+float BlueMagicState::getAperture() {
     return _aperture;
 }
 
-void BlueMagicState::setFocus(float focus)
-{
+void BlueMagicState::setFocus(float focus) {
     _focus = focus;
 }
 
-float BlueMagicState::getFocus()
-{
+float BlueMagicState::getFocus() {
     return _focus;
 }
 
-void BlueMagicState::setZoom(int16_t zoom)
-{
+void BlueMagicState::setZoom(int16_t zoom) {
     _zoom = zoom;
 }
 
-int16_t BlueMagicState::getZoom()
-{
+int16_t BlueMagicState::getZoom() {
     return _zoom;
 }
 
-void BlueMagicState::setIso(uint32_t iso)
-{
+void BlueMagicState::setIso(uint32_t iso) {
     _iso = iso;
 }
 
-int32_t BlueMagicState::getIso()
-{
+int32_t BlueMagicState::getIso() {
     return _iso;
 }
 
-void BlueMagicState::setShutter(int32_t shutter)
-{
+void BlueMagicState::setShutter(int32_t shutter) {
     _shutter = shutter;
 }
 
-float BlueMagicState::getShutter()
-{
+float BlueMagicState::getShutter() {
     return _shutter;
 }
 
-void BlueMagicState::setCodec(int8_t codec)
-{
+void BlueMagicState::setCodec(int8_t codec) {
     _codec = codec;
 }
 
-int8_t BlueMagicState::getCodec()
-{
+int8_t BlueMagicState::getCodec() {
     return _codec;
 }
 
-void BlueMagicState::setQuality(int8_t quality)
-{
+void BlueMagicState::setQuality(int8_t quality) {
     _quality = quality;
 }
 
-int8_t BlueMagicState::getQuality()
-{
+int8_t BlueMagicState::getQuality() {
     return _quality;
 }
 
-void BlueMagicState::setFrameRate(int16_t frameRate)
-{
+void BlueMagicState::setFrameRate(int16_t frameRate) {
     _frameRate = frameRate;
 }
 
-int16_t BlueMagicState::getFrameRate()
-{
+int16_t BlueMagicState::getFrameRate() {
     return _frameRate;
 }
 
-void BlueMagicState::setSensorFrameRate(int16_t sensorFrameRate)
-{
+void BlueMagicState::setSensorFrameRate(int16_t sensorFrameRate) {
     _sensorFrameRate = sensorFrameRate;
 }
 
-int16_t BlueMagicState::getSensorFrameRate()
-{
+int16_t BlueMagicState::getSensorFrameRate() {
     return _sensorFrameRate;
 }
 
-void BlueMagicState::setFrameWidth(int16_t width)
-{
+void BlueMagicState::setFrameWidth(int16_t width) {
     _frameWidth = width;
 }
 
-int16_t BlueMagicState::getFrameWidth()
-{
+int16_t BlueMagicState::getFrameWidth() {
     return _frameWidth;
 }
 
-void BlueMagicState::setFrameHeight(int16_t height)
-{
+void BlueMagicState::setFrameHeight(int16_t height) {
     _frameHeight = height;
 }
 
-int16_t BlueMagicState::getFrameHeight()
-{
+int16_t BlueMagicState::getFrameHeight() {
     return _frameHeight;
 }
 
-void BlueMagicState::setFormatFlags(int8_t flags)
-{
+void BlueMagicState::setFormatFlags(int8_t flags) {
     _flags = flags;
 }
 
-int8_t BlueMagicState::getFormatFlags()
-{
+int8_t BlueMagicState::getFormatFlags() {
     return _flags;
 }
 
-void BlueMagicState::setWhiteBalance(int16_t whiteBalance)
-{
+void BlueMagicState::setWhiteBalance(int16_t whiteBalance) {
     _whiteBalance = whiteBalance;
 }
 
-int16_t BlueMagicState::getWhiteBalance()
-{
+int16_t BlueMagicState::getWhiteBalance() {
     return _whiteBalance;
 }
 
-void BlueMagicState::setTint(int16_t tint)
-{
+void BlueMagicState::setTint(int16_t tint) {
     _tint = tint;
 }
 
-int16_t BlueMagicState::getTint()
-{
+int16_t BlueMagicState::getTint() {
     return _tint;
 }

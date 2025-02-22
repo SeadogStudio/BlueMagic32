e2f7c803fc4d926fa75d5e925886c2bf23d7907d
SeadogStudio <yort136@gmail.com>
2025-02-22 10:46:55 +0700

    Update BlueMagicCameraConnection.h

--- src/BlueMagicCameraConnection.h
+++ src/BlueMagicCameraConnection.h
@@ -1,85 +1,65 @@
-
-#ifndef BLEDevice_h
-#include <BLEDevice.h>
-#endif
-
-#ifndef Preferences_h
-#include <Preferences.h>
-#define PREF_INCLUDED false
-#endif
-
 #ifndef BlueMagicCameraConnection_h
-
 #define BlueMagicCameraConnection_h
 
 #include "Arduino.h"
+#include <BLEDevice.h>
+#include <Preferences.h>
 #include "BlueMagicState.h"
 #include "BlueMagicCameraController.h"
+#include "MyAdvertisedDeviceCallbacks.h"
+#include "MySecurity.h"
 
-enum CONNECTION_STATE
-{
-  CAMERA_CONNECTED = 1,
-  CAMERA_DISCONNECTED = 2,
-  CAMERA_CONNECTING = 3
+enum CONNECTION_STATE {
+    CAMERA_CONNECTED = 1,
+    CAMERA_DISCONNECTED = 2,
+    CAMERA_CONNECTING = 3
 };
 
-class BlueMagicCameraConnection
-{
-
+class BlueMagicCameraConnection {
 public:
-  BlueMagicCameraConnection();
-  ~BlueMagicCameraConnection();
+    BlueMagicCameraConnection();
+    ~BlueMagicCameraConnection();
 
-  void begin();
-  void begin(String name);
-  void begin(String name, Preferences &pref);
+    void begin(const String& name);
+    void begin(const String& name, Preferences& pref);
 
-  bool scan(bool active, int duration);
+    bool scan(bool active, int duration);
 
-  BlueMagicCameraController *connect();
-  BlueMagicCameraController *connect(uint8_t index);
+    BlueMagicCameraController* connect();
+    BlueMagicCameraController* connect(uint8_t index);
 
-  void disconnect();
+    void disconnect();
+    void clearPairing();
 
-  void clearPairing();
-
-  bool available();
+    bool available() const;
+    int connected() const;
+    bool getAuthentication() const;
+    BLEAddress getCameraAddress() const;
 
 private:
-  String _name;
-  Preferences *_pref;
-  bool _init = false;
+    String _name;
+    Preferences* _pref;
+    bool _init = false;
+    bool _authenticated;
+    int _connected;
 
-  bool _authenticated;
-  int _connected;
+    BLEDevice _device;
+    BLEClient* _client;
+    BLEAddress _cameraAddress;
 
-  BLEDevice _device;
-  BLEClient *_client;
+    BLERemoteCharacteristic* _outgoingCameraControl;
+    BLERemoteCharacteristic* _incomingCameraControl;
+    BLERemoteCharacteristic* _timecode;
+    BLERemoteCharacteristic* _cameraStatus;
+    BLERemoteCharacteristic* _deviceName;
 
-  BLEAddress *_cameraAddress = nullptr;
+    BLEScan* _bleScan;
+    BlueMagicCameraController* _cameraControl;
 
-  static BLERemoteCharacteristic *_outgoingCameraControl;
-  static BLERemoteCharacteristic *_incomingCameraControl;
-  static BLERemoteCharacteristic *_timecode;
-  static BLERemoteCharacteristic *_cameraStatus;
-  static BLERemoteCharacteristic *_deviceName;
-
-  BLEScan *_bleScan;
-
-  BlueMagicCameraController *_cameraControl = nullptr;
-
-  bool connectToServer(BLEAddress address);
-
-  int connected();
-  bool authenticated();
-
-  void setController();
-
-  void setState(CONNECTION_STATE state);
-  void setAuthentication(bool authenticated);
-  bool getAuthentication();
-  void setCameraAddress(BLEAddress address);
-  BLEAddress *getCameraAddress();
+    bool connectToServer(const BLEAddress& address);
+    void setController();
+    void setState(CONNECTION_STATE state);
+    void setAuthentication(bool authenticated);
 };
 
-#endif
+#endif // BlueMagicCameraConnection_h

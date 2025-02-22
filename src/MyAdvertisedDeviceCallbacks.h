f2d4d26bbe6df8ffffc46ada239f9ee77bf4fe50
SeadogStudio <yort136@gmail.com>
2025-02-22 11:01:06 +0700

    Create MyAdvertisedDeviceCallbacks.h

---
+++ src/MyAdvertisedDeviceCallbacks.h
@@ -0,0 +1,18 @@
+#ifndef MYADVERTISEDEVICE_H
+#define MYADVERTISEDEVICE_H
+
+#include <BLEAdvertisedDevice.h>
+#include <BLEAdvertisedDeviceCallbacks.h>
+#include "BlueMagicCameraConnection.h" // Needed because it takes a pointer to the BlueMagicCameraConnection class
+
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+public:
+    MyAdvertisedDeviceCallbacks(BlueMagicCameraConnection* connection);
+    void onResult(BLEAdvertisedDevice advertisedDevice);
+
+private:
+    BlueMagicCameraConnection* _connection;
+
+};
+
+#endif // MYADVERTISEDEVICE_H

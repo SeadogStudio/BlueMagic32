23465fada85c1e067889707dd8461d861051b563
SeadogStudio <yort136@gmail.com>
2025-02-22 15:29:42 +0700

    Update BlueMagic32.h

--- src/BlueMagic32.h
+++ src/BlueMagic32.h
@@ -1,6 +1,23 @@
-#include "BlueMagicCameraConnection.h"
-#include "BlueMagicCameraController.h"
-#include "MyAdvertisedDeviceCallbacks.h"
+// BlueMagic32.h
 
-BlueMagicCameraConnection BMDConnection;
-BlueMagicCameraController *BMDControl;
+#ifndef BlueMagic32_h
+#define BlueMagic32_h
+
+#include "Arduino.h"
+#include "BlueMagicCameraConnection.h"
+
+class BlueMagic32 {
+public:
+  BlueMagic32();
+  void begin(const char* deviceName);
+  void startCameraConnection(void (*cameraConnectedCallback)(), void (*cameraDisconnectedCallback)(), void (*shutterPressedCallback)());
+  void stopCameraConnection();
+  void sendShutterPress();
+  bool isCameraConnected();
+
+private:
+  BlueMagicCameraConnection cameraConnection;
+  bool cameraConnected;
+};
+
+#endif
